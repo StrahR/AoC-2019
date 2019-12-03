308c17518176aa6f4c7e00fa90d5c49d4ecd8c8e
Rok Strah <rok.strah@student.fmf.uni-lj.si>
2019-12-03 22:30:12 +0100

    solved day 3

---
+++ day_03.ml
@@ -0,0 +1,69 @@
+module IntPairs =
+struct
+  type t = int * int
+  let compare (x0, y0) (x1, y1) =
+    match Stdlib.compare x0 x1 with
+    | 0 -> Stdlib.compare y0 y1
+    | c -> c
+end
+module PairsMap = Map.Make(IntPairs)
+
+let naloga1 points1 =
+  let rec aux best = function
+    | [] -> best
+    | ((a, b as x), _) :: xs ->
+      if x <> (0, 0) && PairsMap.mem x points1
+      then aux (min best ((abs a) + (abs b))) xs
+      else aux best xs
+  in aux 1000000
+
+let naloga2 points1 =
+  let rec aux best = function
+    | [] -> best
+    | ((a, b as x), d) :: xs ->
+      if x <> (0, 0) && PairsMap.mem x points1
+      then aux (min best ((PairsMap.find x points1) + d)) xs
+      else aux best xs
+  in aux 1000000
+
+let _ =
+  let build_list =
+    let rec add_line acc k start length dir n = match dir with
+      | 'R' -> if length >= 0 then add_line (PairsMap.add (start, k) n acc) k (start+1) (length-1) dir (n+1) else acc
+      | 'L' -> if length >= 0 then add_line (PairsMap.add (start, k) n acc) k (start-1) (length-1) dir (n+1) else acc
+      | 'D' -> if length >= 0 then add_line (PairsMap.add (k, start) n acc) k (start-1) (length-1) dir (n+1) else acc
+      | 'U' -> if length >= 0 then add_line (PairsMap.add (k, start) n acc) k (start+1) (length-1) dir (n+1) else acc
+      | _ -> PairsMap.empty
+    in let rec aux acc x y n = function
+        | [] -> acc
+        | s :: ss ->
+          let dir = String.get s 0 in
+          let len = int_of_string (String.sub s 1 (String.length s - 1)) in
+          (* print_endline "one line"; *)
+          match dir with
+          | 'R' -> aux (add_line acc y x len dir n) (x + len) y (n+len) ss
+          | 'L' -> aux (add_line acc y x len dir n) (x - len) y (n+len) ss
+          | 'D' -> aux (add_line acc x y len dir n) x (y - len) (n+len) ss
+          | 'U' -> aux (add_line acc x y len dir n) x (y + len) (n+len) ss
+          | _ -> PairsMap.empty
+    in aux PairsMap.empty 0 0 0
+  and preberi_datoteko ime_datoteke =
+    let chan = open_in ime_datoteke in
+    let vsebina = really_input_string chan (in_channel_length chan) in
+    close_in chan;
+    vsebina
+  and izpisi_datoteko ime_datoteke vsebina =
+    let chan = open_out ime_datoteke in
+    output_string chan vsebina;
+    close_out chan
+  in
+  let vsebina_datoteke = String.split_on_char '\n' (preberi_datoteko "day_03.in") in
+  let path1 = List.nth vsebina_datoteke 0 in
+  let path2 = List.nth vsebina_datoteke 1 in
+  let points1 = build_list (String.split_on_char ',' path1) in
+  let points2 = build_list (String.split_on_char ',' path2) in
+  let odgovor1 = naloga1 points1 (PairsMap.bindings points2)
+  and odgovor2 = naloga2 points1 (PairsMap.bindings points2)
+  in
+  izpisi_datoteko "day_03_1.out" (string_of_int odgovor1);
+  izpisi_datoteko "day_03_2.out" (string_of_int odgovor2)
